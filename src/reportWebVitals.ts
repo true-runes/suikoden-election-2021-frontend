6b6fbf58d4373a29b909b9a9b6bd918d0dbb55fd
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-05-29 22:01:18 +0900

    feat: 🎸 faviconを追加した

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,17 +1,17 @@
-import { ReportHandler } from 'web-vitals';
+import { ReportHandler } from 'web-vitals'
 
 // TODO: Missing return type on function  @typescript-eslint/explicit-module-boundary-types
 // eslint-disable-next-line
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+      getCLS(onPerfEntry)
+      getFID(onPerfEntry)
+      getFCP(onPerfEntry)
+      getLCP(onPerfEntry)
+      getTTFB(onPerfEntry)
+    })
   }
-};
+}
 
-export default reportWebVitals;
+export default reportWebVitals
