a9b9524a26929a9fcc17f9d2a916a198b3cf3bef
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-06-02 20:11:17 +0900

    feat: 🎸 Image コンポーネントを Chakra から読み込み、GIF画像を追加

--- src/NowLoading.tsx
+++ src/NowLoading.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Image } from 'semantic-ui-react'
+import { Image } from '@chakra-ui/react'
 
-import NowLoadingGif from './images/now_loading.gif'
+import NowLoadingGif from './images/now_loading_bounce.gif'
 
 export const NowLoading = () => {
   return (
     <div>
-      <Image src={NowLoadingGif} size="mini" />
+      <Image src={NowLoadingGif} boxSize="64px" />
     </div>
   )
 }
