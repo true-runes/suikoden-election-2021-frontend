cb8a0b3038149c3b30d35e946ec7cb4328a1e8aa
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-06-04 06:32:06 +0900

    style: 💄 ツイート応募完了についての表示場所や文言を微修正

--- src/SearchTweetsFoundNotice.tsx
+++ src/SearchTweetsFoundNotice.tsx
@@ -1,48 +1,46 @@
 import React from 'react'
 import { Box } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
 import { Alert, AlertIcon } from '@chakra-ui/react' // import {
 import { Text } from '@chakra-ui/react'
 
 export const SearchTweetsFoundNotice = (props: any) => {
   return (
-    <Box p={2}>
-      <Alert status="success">
-        <AlertIcon />
-        <Text align="left">
-          {props.searchedUsername} (@{props.searchedScreenName})
-          さんのツイートが {props.numberOfFoundTweets}件 見つかりました。
-        </Text>
-      </Alert>
-      <Box p={2}></Box>
-      <Alert status="info">
-        <AlertIcon />
-        <Text align="left">
-          削除済みツイートがある場合には上記の数字が正しくないことがありますが、集計は正しく行われています。
-        </Text>
-      </Alert>
-      <Box p={2}></Box>
-      <Alert status="info">
-        <AlertIcon />
-        <Text align="left">
-          検索結果にツイートが表示されれば、そのツイートの応募は完了しています。
-        </Text>
-      </Alert>
-      <Box p={2}></Box>
-      <Alert status="info">
-        <AlertIcon />
-        <Text align="left">
-          ご自身のツイートの全てが表示されていない場合は
-          <a
-            href="https://twitter.com/gensosenkyo"
-            target="_blank"
-            rel="noreferrer"
-            style={{ textDecoration: 'underline' }}
-          >
-            主催宛DM
-          </a>
-          でご連絡下さい。
-        </Text>
-      </Alert>
-    </Box>
+    <Container maxW="container.xl">
+      <hr style={{ margin: '0 0 13px 0' }} />
+
+      <Box p={2}>
+        <Alert status="success">
+          <AlertIcon />
+          <Text align="left">
+            {props.searchedUsername} (@{props.searchedScreenName})
+            さんのツイートが {props.numberOfFoundTweets}件 見つかりました。
+          </Text>
+        </Alert>
+        <Box p={2}></Box>
+        <Alert status="info">
+          <AlertIcon />
+          <Text align="left">
+            削除済みツイートがある場合には上記の数字が正しくないことがありますが、集計は正しく行われています。
+          </Text>
+        </Alert>
+        <Box p={2}></Box>
+        <Alert status="info">
+          <AlertIcon />
+          <Text align="left">
+            ご自身のツイートの全てが表示されていない場合は
+            <a
+              href="https://twitter.com/gensosenkyo"
+              target="_blank"
+              rel="noreferrer"
+              style={{ textDecoration: 'underline' }}
+            >
+              主催宛DM
+            </a>
+            でご連絡下さい。
+          </Text>
+        </Alert>
+      </Box>
+    </Container>
   )
 }
